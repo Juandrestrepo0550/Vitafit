32306a80c21d14ccbf02b25014388eb2601c8410
Xxdarknight0xX <darknight16253@gmail.com>
2025-04-03 15:54:17 -0500

    LandingPage - Diego Garcia
    creacion del landing page, configuracion del css, javascript y vinculaciones con python

--- VitafitPy/Vitafit/Vitafit/urls.py
+++ VitafitPy/Vitafit/Vitafit/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Vitafit project.
+URL configuration for vitafit project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+
+#para vincular las hojas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app.com/', include('landingpage.urls')),
+    
 ]
