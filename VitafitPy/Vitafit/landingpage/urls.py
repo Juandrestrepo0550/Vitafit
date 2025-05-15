404496ea9dd5610e7ab6d3dec83dce7c13a33c8d
Juandrestrepo0550 <144486184+Juandrestrepo0550@users.noreply.github.com>
2025-05-15 17:13:04 -0500

    Validacion
    Juan david

--- VitafitPy/Vitafit/landingpage/urls.py
+++ VitafitPy/Vitafit/landingpage/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('val_correo/', views.val_correo, name='val_correo'),
     path('inicio_sesion/',views.inicio_sesion, name='inicio_sesion'),
     path('logout/', views.cerrar_sesion, name='logout'),
     path('adminpage/', views.adminpage, name='adminpage'),
