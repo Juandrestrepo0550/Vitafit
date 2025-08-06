44d719961bf394e2659ca4799bc77ad13a2059cb
sami0047g <samirahmedmazloum@gmail.com>
2025-08-05 19:46:00 -0500

    Diseño de inicio de sesion de google
    Samir Ahmed Jr Mazloum Mosquera

--- VitafitPy/Vitafit/Vitafit/settings.py
+++ VitafitPy/Vitafit/Vitafit/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    'widget_tweaks',
 ]
 
 SITE_ID = 1
@@ -73,7 +74,7 @@ ROOT_URLCONF = 'Vitafit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
